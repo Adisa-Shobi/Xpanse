c350eeeb8c8103041ae2375b8fa6dcf0b7f4147b
Shobi <s.oadisa.dev@gmail.com>
2024-10-27 13:18:47 +0200

    update(compile): changes compiled

--- xpanse_app/lib/routes/app_pages.dart
+++ xpanse_app/lib/routes/app_pages.dart
@@ -3,7 +3,8 @@ import 'package:xpanse_app/bindings/bindings.dart';
 import 'package:xpanse_app/views/auth/login.dart';
 import 'package:xpanse_app/views/auth/signup.dart';
 import 'package:xpanse_app/views/home/home.dart';
-import 'package:xpanse_app/views/onboarding/onboarding.dart';
+import 'package:xpanse_app/views/onboarding/onboarding_1.dart';
+import 'package:xpanse_app/views/onboarding/onboarding_2.dart';
 import 'route_names.dart';
 // Import your page components and bindings here
 
@@ -23,8 +24,13 @@ class AppPages {
       page: () => SignUpPage(),
     ),
     GetPage(
-      name: RouteNames.onboarding,
-      page: () => OnboardingPage(),
+      name: RouteNames.onboarding1,
+      page: () => OnboardingScreen(),
+      // binding: ProfileBinding(),
+    ),
+    GetPage(
+      name: RouteNames.onboarding2,
+      page: () => OnboardingScreen2(),
       // binding: ProfileBinding(),
     ),
   ];
--- xpanse_app/lib/views/onboarding/onboarding_2.dart
+++ xpanse_app/lib/views/onboarding/onboarding_2.dart
@@ -1,97 +1,95 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:xpanse_app/routes/route_names.dart';
 
-
 class OnboardingScreen2 extends StatelessWidget {
   const OnboardingScreen2({super.key});
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-         crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              margin: const EdgeInsets.only(top: 100.0),
-              child: Image.asset(
-                'assets/Onboarding 2.png',
-                width: 400,
-                height: 250,
-              ),
-            ), //
-
-
-            const Text(
-              'Manage your money\n            wisely',
-              style: const TextStyle(
-                fontFamily: 'Montserrat',
-                fontSize: 24,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            const Text(
-              '     Track your money flows, balance, and \n       make everyday transactions on the go.',
-              style: const TextStyle(
-                color: const Color(0x80000000),
-                fontFamily: 'Montserrat',
-                fontSize: 14,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-           
-            // button one
-            Container(
-              width: 327,
-              height: 56.0,
-              margin: const EdgeInsets.symmetric(vertical: 20.0),
-              child: ElevatedButton.icon(
-                onPressed: () {
-                  Get.toNamed(RouteNames.login);
-                },
-                label: const Text(
-                  'Get started',
-                  style: const TextStyle(fontFamily: 'Montserrat', fontSize: 16, fontWeight: FontWeight.w500),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.only(top: 100.0),
+                child: Image.asset(
+                  'assets/images/Onboarding 2.png',
+                  width: 400,
+                  height: 250,
                 ),
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  backgroundColor: const Color(0xFF40196C),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
+              ), //
+
+              const Text(
+                'Manage your money\n            wisely',
+                style: const TextStyle(
+                  fontFamily: 'Montserrat',
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              const Text(
+                '     Track your money flows, balance, and \n       make everyday transactions on the go.',
+                style: const TextStyle(
+                  color: const Color(0x80000000),
+                  fontFamily: 'Montserrat',
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+
+              // button one
+              Container(
+                width: 327,
+                height: 56.0,
+                margin: const EdgeInsets.symmetric(vertical: 20.0),
+                child: ElevatedButton.icon(
+                  onPressed: () {
+                    Get.toNamed(RouteNames.login);
+                  },
+                  label: const Text(
+                    'Get started',
+                    style: const TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: const Color(0xFF40196C),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
                   ),
                 ),
               ),
-            ),
 
-
-            //button 2
-            SizedBox(
-              width: 327.0,
-              height: 56.0,
-              child: ElevatedButton.icon(
-                onPressed: () {},
-                label: const Text(
-                  'Sign-in',
-                  style: const TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500
+              //button 2
+              SizedBox(
+                width: 327.0,
+                height: 56.0,
+                child: ElevatedButton.icon(
+                  onPressed: () {},
+                  label: const Text(
+                    'Sign-in',
+                    style: const TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
                   ),
-                ),
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black,
-                  backgroundColor: const Color(0xFFF0F5FF),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    backgroundColor: const Color(0xFFF0F5FF),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ), //children,
       ),
     );
