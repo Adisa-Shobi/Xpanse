7c2f73da19b6f0b80177869ea210380bef7fde61
MJLEGION <m.george@alustudent.com>
2024-10-27 13:09:24 +0100

    splash screen

---
+++ xpanse_app/lib/views/splash/splash.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'onboarding_screen1.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: const SplashScreen(),
+    );
+  }
+}
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Wait for 3 seconds and navigate to the onboarding screen
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const OnboardingScreen()),
+      );
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF40196C),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/Xpanse.png'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
